77f59144c52a5cf3beed7e9f5a7764976a17aae6
Henrik Nårstad <henrik.narstad@knowit.no>
2016-12-27 12:06:00 +0100

    load secrets from environment

---
+++ components/settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from os import environ
+
+debug=True
+
+secret_key = environ["SECRET_KEY"]
+google_client_id = environ["GOOGLE_CLIENT_ID"]
+google_client_secret = environ["GOOGLE_CLIENT_SECRET"]
+redirect_uri = "/login/verify"
+
+db_credentials = {
+    "dbname": environ["DB_NAME"],
+    "user": environ["DB_USER"],
+    "host": environ["DB_HOST"],
+    "port": environ["DB_PORT"],
+    "password": environ["DB_PASSWORD"],
+}
